020807439ca90997724ea700af51883a4169fbde
shihab <game123ahmed@gmail.com>
2022-04-25 16:27:19 +0600

    navbar have finished

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import {FaSwimmingPool } from 'react-icons/fa'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import { AiOutlineCloseCircle} from 'react-icons/ai'
 
 
 const Navbar=()=>{
+
+    const [click,setClick] = useState(false)
+    const handleClick=()=>setClick(!click)
     return(
         <div className='navbar_section'>
           <div className='container'>
              <h2><span>swi<FaSwimmingPool/>ingly</span></h2>
-             <ul className='nav__menu'> 
+
+             <ul className={click?'nav__menu active':'nav__menu'}> 
                <li>
                    <a href='#'>Home</a>
                </li>
                <li>
-                   <a href='#'>Home</a>
+                   <a href='#'>About</a>
                </li>
                <li>
-                   <a href='#'>Home</a>
+                   <a href='#'>Menu</a>
                </li>
                <li>
-                   <a href='#'>Home</a>
+                   <a href='#'>Contact</a>
                </li>
 
+               <button className='btn'>Sign-In</button>
+
              </ul>
-             <div className='namburger'>
-                    <GiHamburgerMenu/>
+             <div className='namburger' onClick={handleClick}>
+                   {click? (<AiOutlineCloseCircle className='icon'/>):(<GiHamburgerMenu className='icon'/>)}
              </div>
 
           </div>
